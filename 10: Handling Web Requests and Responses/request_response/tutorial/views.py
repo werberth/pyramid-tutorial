2f9b393adf5e6cf930f8217593b0ac6b94654270
Werberth Vinícius <werberthvinicius@gmail.com>
2018-02-11 17:19:09 -0300

    Redirecting view home to plain view, and modifying plain view response.

--- 10: Handling Web Requests and Responses/request_response/tutorial/views.py
+++ 10: Handling Web Requests and Responses/request_response/tutorial/views.py
@@ -1,18 +1,24 @@
+from pyramid.httpexceptions import HTTPFound
+from pyramid.response import Response
 from pyramid.view import (
     view_config,
     view_defaults
 )
 
 
-@view_defaults(renderer='home.pt')
 class TutorialViews:
     def __init__(self, request):
         self.request = request
 
     @view_config(route_name='home')
     def home(self):
-        return {'name': 'Home View'}
+        return HTTPFound(location='/plain')
 
-    @view_config(route_name='hello')
-    def hello(self):
-        return {'name': 'Hello View'}
+    @view_config(route_name='plain')
+    def plain(self):
+        name = self.request.params.get('name', 'No Name Provided')
+        body = 'URL %s with name: %s' % (self.request.url, name)
+        return Response(
+            content_type='text/plain',
+            body=body
+        )
