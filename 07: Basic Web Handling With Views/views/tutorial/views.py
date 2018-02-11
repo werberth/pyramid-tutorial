6a43c4d775e3e99368310b9dfed2fbf9974d6800
Werberth Vinícius <werberthvinicius@gmail.com>
2018-02-11 02:23:16 -0300

    Adding hello view

--- 07: Basic Web Handling With Views/views/tutorial/views.py
+++ 07: Basic Web Handling With Views/views/tutorial/views.py
@@ -6,3 +6,9 @@ from pyramid.view import view_config
 @view_config(route_name='home')
 def home(request):
     return Response('<body>Visit <a href="/howdy">hello</a></body>')
+
+
+# /howdy
+@view_config(route_name='hello')
+def hello(request):
+    return Response('<body>Go back <a href="/">home</a></body>')
