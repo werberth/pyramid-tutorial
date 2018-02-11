2f53d2e22fa30c9c8700b0b65eb613aba9abde87
Werberth Vinícius <werberthvinicius@gmail.com>
2018-02-11 20:12:34 -0300

    Refactoring hello view and adding hello view

--- 12: Templating With jinja2/jinja2/tutorial/views.py
+++ 12: Templating With jinja2/jinja2/tutorial/views.py
@@ -6,17 +6,15 @@ from pyramid.view import (
 )
 
 
-@view_defaults(renderer='home.pt')
+@view_defaults(renderer='home.jinja2')
 class TutorialViews:
     def __init__(self, request):
         self.request = request
 
     @view_config(route_name='home')
     def home(self):
-        first = self.request.matchdict['first']
-        last = self.request.matchdict['last']
-        return {
-            'name': 'Home View',
-            'first': first,
-            'last': last
-        }
+        return {'name': 'Home View'}
+
+    @view_config(route_name='hello')
+    def hello(self):
+        return {'name': 'Hello View'}
