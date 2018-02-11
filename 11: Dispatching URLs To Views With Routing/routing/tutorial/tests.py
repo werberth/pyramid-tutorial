e433e1f8361fb18e25196aa12548ed26d713a050
Werberth Vinícius <werberthvinicius@gmail.com>
2018-02-11 19:24:08 -0300

    Deleting test_plain_without_name and test_plain_with_name from TutorialViewTests and TutorialFunctionalTests

--- 11: Dispatching URLs To Views With Routing/routing/tutorial/tests.py
+++ 11: Dispatching URLs To Views With Routing/routing/tutorial/tests.py
@@ -18,22 +18,6 @@ class TutorialViewTests(unittest.TestCase):
         response = inst.home()
         self.assertEqual(response.status, '302 Found')
 
-    def test_plain_without_name(self):
-        from .views import TutorialViews
-
-        request = testing.DummyRequest()
-        inst = TutorialViews(request)
-        response = inst.plain()
-        self.assertIn(b'No Name Provided', response.body)
-
-    def test_plain_with_name(self):
-        from .views import TutorialViews
-
-        request = testing.DummyRequest()
-        request.GET['name'] = 'Jane Doe'
-        inst = TutorialViews(request)
-        response = inst.plain()
-        self.assertIn(b'Jane Doe', response.body)
 
 class TutorialFunctionalTests(unittest.TestCase):
     def setUp(self):
@@ -43,11 +27,3 @@ class TutorialFunctionalTests(unittest.TestCase):
         from webtest import TestApp
 
         self.testapp = TestApp(app)
-
-    def test_plain_without_name(self):
-        res = self.testapp.get('/plain', status=200)
-        self.assertIn(b'No Name Provided', res.body)
-
-    def test_plain_with_name(self):
-        res = self.testapp.get('/plain?name=Jane%20Doe', status=200)
-        self.assertIn(b'Jane Doe', res.body)
