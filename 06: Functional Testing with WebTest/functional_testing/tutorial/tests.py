3c2ba400d71c2457fec91dbd534e04440403d41c
Werberth Vinícius <werberthvinicius@gmail.com>
2018-02-11 01:10:24 -0300

    Adding TutorialFunctionalTests

--- 06: Functional Testing with WebTest/functional_testing/tutorial/tests.py
+++ 06: Functional Testing with WebTest/functional_testing/tutorial/tests.py
@@ -16,3 +16,15 @@ class TutorialViewTests(unittest.TestCase):
         request = testing.DummyRequest()
         response = hello_world(request)
         self.assertEqual(response.status_code, 200)
+
+
+class TutorialFunctionalTests(unittest.TestCase):
+    def setUp(self):
+        from tutorial import main
+        app = main({})
+        from webtest import TestApp
+        self.testapp = TestApp(app)
+
+    def test_hello_world(self):
+        res = self.testapp.get('/', status=200)
+        self.assertIn(b'<h1>Hello World!</h1>', res.body)
