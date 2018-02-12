15d48b9017461cf2bdc43edf93b86876e180c960
Werberth Vinícius <werberthvinicius@gmail.com>
2018-02-12 01:47:33 -0300

    Make the tests pass to test_home unit test

--- 15: More With View Classes/more_view_classes/tutorial/tests.py
+++ 15: More With View Classes/more_view_classes/tutorial/tests.py
@@ -11,10 +11,11 @@ class TutorialViewTests(unittest.TestCase):
         testing.tearDown()
 
     def test_home(self):
-        from .views import home
+        from .views import TutorialViews
 
         request = testing.DummyRequest()
-        response = home(request)
+        inst = TutorialViews(request)
+        response = inst.home()
         self.assertIn('Home View', response['name'])
 
     def test_hello(self):
