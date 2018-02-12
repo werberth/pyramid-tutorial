65625f9f52169f43aabb10c2d36f2a7d2054e1a7
Werberth Vinícius <werberthvinicius@gmail.com>
2018-02-11 21:09:05 -0300

    add a call config.add_static_view in static_assets/tutorial/__init__.py

--- 13: CSS, JS, Images Files With Static Assets/static_assets/tutorial/__init__.py
+++ 13: CSS, JS, Images Files With Static Assets/static_assets/tutorial/__init__.py
@@ -6,5 +6,6 @@ def main(global_config, **settings):
     config.include('pyramid_chameleon')
     config.add_route('home', '/')
     config.add_route('hello', '/howdy')
+    config.add_static_view(name='static', path='tutorial:static')
     config.scan('.views')
     return config.make_wsgi_app()
